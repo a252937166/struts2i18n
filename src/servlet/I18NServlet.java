b140de6c13c5b4229c13809f7daa9ada3e6e3d57
a252937166 <250757958@qq.com>
2016-05-11 00:35:30 +0800

    国际化

--- src/servlet/I18NServlet.java
+++ src/servlet/I18NServlet.java
@@ -1,7 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Locale;
+import java.util.Properties;
 
 /**
  * Created by OyDn on 2016/5/10.
@@ -12,9 +14,14 @@ public class I18NServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        Locale locale = Locale.getDefault();
-        System.out.println(locale);
+//        Locale locale = Locale.getDefault();
+//        System.out.println(locale);
         Locale locale1 = request.getLocale();
         System.out.println(locale1);
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        Properties prop = new Properties();
+        prop.load(this.getClass().getResourceAsStream("/aa_" + locale1.toString() +".properties"));
+        out.println(prop.getProperty("aa"));
     }
 }
